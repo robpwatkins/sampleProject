2d550bba83b0b598fea30767bdf5ccb971be332a
Rob W <robpwatkins@gmail.com>
2020-10-08 13:31:53 -0500

    thursdayyy

--- encounter97.js
+++ encounter97.js
@@ -4,4 +4,4 @@ const day = moment().format('dddd');
 
 console.log(`When you have a party on a ${day} night, it's a ${day} night party.`);
 
-console.log(`Trumpets shall sound`)
+console.log(`Trumpets will sound and all the dead shall rise and walk `)
