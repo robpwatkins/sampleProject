cd7b71b2ef955134bb69a7c1d2331bee8e968a70
Rob W <robpwatkins@gmail.com>
2020-10-08 13:31:27 -0500

    woops

--- encounter97.js
+++ encounter97.js
@@ -3,3 +3,5 @@ const moment = require('moment');
 const day = moment().format('dddd');
 
 console.log(`When you have a party on a ${day} night, it's a ${day} night party.`);
+
+console.log(`Trumpets shall sound`)
